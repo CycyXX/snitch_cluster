89533a77833481680d168dc5cb7112e6f055629f
cdurrer <cdurrer@iis.ee.ethz.ch>
2025-08-22 16:27:22 +0200

    Code cleanup

--- hw/snitch_cluster/src/konark_top.sv
+++ hw/snitch_cluster/src/konark_top.sv
@@ -10,10 +10,7 @@
 
 
 module konark_top
-  // import floo_pkg::*;
-  // import floo_picobello_noc_pkg::*;
   import snitch_cluster_pkg::*;
-  // import picobello_pkg::*;
 (
   input  logic                                   clk_i,
   input  logic                                   rst_ni,
@@ -34,10 +31,6 @@ module konark_top
   input  snitch_cluster_pkg::wide_out_resp_t     wide_out_resp_i,
   input  snitch_cluster_pkg::wide_in_req_t       wide_in_req_i,
   output snitch_cluster_pkg::wide_in_resp_t      wide_in_resp_o
-  // output snitch_cluster_pkg::narrow_out_req_t    narrow_ext_req_o,
-  // input  snitch_cluster_pkg::narrow_out_resp_t   narrow_ext_resp_i,
-  // input  snitch_cluster_pkg::tcdm_dma_req_t [1-1:0] tcdm_ext_req_i,
-  // output snitch_cluster_pkg::tcdm_dma_rsp_t [1-1:0] tcdm_ext_resp_o
 );
 
   ////////////////////
@@ -205,127 +198,5 @@ module konark_top
     .hwpe_ctrl_rsp_o(hwpectrl_rsp),
     .hwpe_evt_o     (mxip)
   );
-
-  ////////////
-  // Router //
-  ////////////
-
-  // floo_req_t [Eject:North] router_floo_req_out, router_floo_req_in;
-  // floo_rsp_t [Eject:North] router_floo_rsp_out, router_floo_rsp_in;
-  // floo_wide_t [Eject:North] router_floo_wide_out, router_floo_wide_in;
-
-  // floo_nw_router #(
-  //   .AxiCfgN     (AxiCfgN),
-  //   .AxiCfgW     (AxiCfgW),
-  //   .EnMultiCast (RouteCfg.EnMultiCast),
-  //   .RouteAlgo   (RouteCfg.RouteAlgo),
-  //   .NumRoutes   (5),
-  //   .InFifoDepth (2),
-  //   .OutFifoDepth(2),
-  //   .id_t        (id_t),
-  //   .hdr_t       (hdr_t),
-  //   .floo_req_t  (floo_req_t),
-  //   .floo_rsp_t  (floo_rsp_t),
-  //   .floo_wide_t (floo_wide_t)
-  // ) i_router (
-  //   .clk_i,
-  //   .rst_ni,
-  //   .test_enable_i,
-  //   .id_i,
-  //   .id_route_map_i('0),
-  //   .floo_req_i    (router_floo_req_in),
-  //   .floo_rsp_o    (router_floo_rsp_out),
-  //   .floo_req_o    (router_floo_req_out),
-  //   .floo_rsp_i    (router_floo_rsp_in),
-  //   .floo_wide_i   (router_floo_wide_in),
-  //   .floo_wide_o   (router_floo_wide_out)
-  // );
-
-  // assign floo_req_o                      = router_floo_req_out[West:North];
-  // assign router_floo_req_in[West:North]  = floo_req_i;
-  // assign floo_rsp_o                      = router_floo_rsp_out[West:North];
-  // assign router_floo_rsp_in[West:North]  = floo_rsp_i;
-  // assign floo_wide_o                     = router_floo_wide_out[West:North];
-  // assign router_floo_wide_in[West:North] = floo_wide_i;
-
-  // /////////////
-  // // Chimney //
-  // /////////////
-
-  // floo_nw_chimney #(
-  //   .AxiCfgN             (floo_picobello_noc_pkg::AxiCfgN),
-  //   .AxiCfgW             (floo_picobello_noc_pkg::AxiCfgW),
-  //   .ChimneyCfgN         (floo_pkg::ChimneyDefaultCfg),
-  //   .ChimneyCfgW         (floo_pkg::ChimneyDefaultCfg),
-  //   .RouteCfg            (floo_picobello_noc_pkg::RouteCfg),
-  //   .AtopSupport         (1'b1),
-  //   .MaxAtomicTxns       (1),
-  //   .Sam                 (picobello_pkg::SamMcast),
-  //   .id_t                (floo_picobello_noc_pkg::id_t),
-  //   .rob_idx_t           (floo_picobello_noc_pkg::rob_idx_t),
-  //   .hdr_t               (floo_picobello_noc_pkg::hdr_t),
-  //   .sam_rule_t          (picobello_pkg::sam_multicast_rule_t),
-  //   .sam_idx_t           (picobello_pkg::sam_idx_t),
-  //   .mask_sel_t          (picobello_pkg::mask_sel_t),
-  //   .axi_narrow_in_req_t (snitch_cluster_pkg::narrow_out_req_t),
-  //   .axi_narrow_in_rsp_t (snitch_cluster_pkg::narrow_out_resp_t),
-  //   .axi_narrow_out_req_t(snitch_cluster_pkg::narrow_in_req_t),
-  //   .axi_narrow_out_rsp_t(snitch_cluster_pkg::narrow_in_resp_t),
-  //   .axi_wide_in_req_t   (snitch_cluster_pkg::wide_out_req_t),
-  //   .axi_wide_in_rsp_t   (snitch_cluster_pkg::wide_out_resp_t),
-  //   .axi_wide_out_req_t  (snitch_cluster_pkg::wide_in_req_t),
-  //   .axi_wide_out_rsp_t  (snitch_cluster_pkg::wide_in_resp_t),
-  //   .floo_req_t          (floo_picobello_noc_pkg::floo_req_t),
-  //   .floo_rsp_t          (floo_picobello_noc_pkg::floo_rsp_t),
-  //   .floo_wide_t         (floo_picobello_noc_pkg::floo_wide_t),
-  //   .sram_cfg_t          (snitch_cluster_pkg::sram_cfg_t),
-  //   .user_struct_t       (picobello_pkg::mcast_user_t)
-  // ) i_chimney (
-  //   .clk_i               (tile_clk),
-  //   .rst_ni              (tile_rst_n),
-  //   .test_enable_i,
-  //   .id_i,
-  //   .route_table_i       ('0),
-  //   .sram_cfg_i          ('0),
-  //   .axi_narrow_in_req_i (cluster_narrow_out_req),
-  //   .axi_narrow_in_rsp_o (cluster_narrow_out_rsp),
-  //   .axi_narrow_out_req_o(cluster_narrow_in_req),
-  //   .axi_narrow_out_rsp_i(cluster_narrow_in_rsp),
-  //   .axi_wide_in_req_i   (cluster_wide_out_req),
-  //   .axi_wide_in_rsp_o   (cluster_wide_out_rsp),
-  //   .axi_wide_out_req_o  (cluster_wide_in_req),
-  //   .axi_wide_out_rsp_i  (cluster_wide_in_rsp),
-  //   .floo_req_o          (router_floo_req_in[Eject]),
-  //   .floo_rsp_o          (router_floo_rsp_in[Eject]),
-  //   .floo_wide_o         (router_floo_wide_in[Eject]),
-  //   .floo_req_i          (router_floo_req_out[Eject]),
-  //   .floo_rsp_i          (router_floo_rsp_out[Eject]),
-  //   .floo_wide_i         (router_floo_wide_out[Eject])
-  // );
-
-  //////////////////////////
-  // Clock Gating & Reset //
-  //////////////////////////
-
-//   tc_clk_gating i_tc_clk_gating_cluster (
-//     .clk_i,
-//     .en_i     (tile_clk_en_i),
-//     .test_en_i(clk_rst_bypass_i),
-//     .clk_o    (tile_clk)
-//   );
-
-// `ifdef TARGET_XILINX
-//   // Using clk cells makes Vivado flag the reset as a clock tree
-//   assign tile_rst_n = (clk_rst_bypass_i) ? rst_ni : tile_rst_ni;
-// `else
-//   tc_clk_mux2 i_tc_reset_mux (
-//     .clk0_i   (tile_rst_ni),
-//     .clk1_i   (rst_ni),
-//     .clk_sel_i(clk_rst_bypass_i),
-//     .clk_o    (tile_rst_n)
-//   );
-// `endif
-
-
 
 endmodule
--- hw/snitch_cluster/src/snitch_hwpe_subsystem.sv
+++ hw/snitch_cluster/src/snitch_hwpe_subsystem.sv
@@ -14,8 +14,8 @@ module snitch_hwpe_subsystem
   parameter type         tcdm_rsp_t    = logic,
   parameter type         periph_req_t  = logic,
   parameter type         periph_rsp_t  = logic,
-  parameter int unsigned HwpeDataWidth = 288, //256,
-  parameter int unsigned IdWidth       = 2, //8,
+  parameter int unsigned HwpeDataWidth = 288,
+  parameter int unsigned IdWidth       = 2,
   parameter int unsigned NrCores       = 2,    // corresponds to Snitch cluster (ToDo(cdurrer): count DMA cores?)
   parameter int unsigned NrContext     = 2,
   parameter int unsigned PE_H          = 6,    // 6x6 PE array = 36 PEs with 32 MACUs each: 1152 MACUs
